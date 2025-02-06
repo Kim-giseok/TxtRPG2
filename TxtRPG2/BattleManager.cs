ceea74b033d8ab7ab807889c8341e056e0d5491d
Kim-giseok <belbeck@naver.com>
2025-02-06 16:47:39 +0900

    Feat: BattleManager.cs 생성
    기본적인 필드와 미구현 매서드가 포함된 BattleManager클래스를 생성했습니다.

---
+++ TxtRPG2/BattleManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal class BattleManager
+    {
+        Player player;
+        Enemy[] Enemys;
+
+        public BattleManager(Player player)
+        {
+            
+        }
+
+        public void Battle()
+        {
+
+        }
+    }
+}
