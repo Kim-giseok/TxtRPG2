159913a666e5abc75cc59d681a861412abbdf9e2
KwonSumin <rbalstnals1920@gmail.com>
2025-02-06 19:29:56 +0900

    feat 게임 시작 인사 및 메뉴 추가
    게임 시작 인사 추가
    메뉴 추가

---
+++ TxtRPG2/CinsoleUtity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+static class ConsoleUtility
+{
+    
+    //글자 색 변경
+    public static void ColorWrite(string str, ConsoleColor color)
+    {
+        Console.ForegroundColor = color; //텍스트 컬러 설정
+        Console.WriteLine(str);
+        Console.ResetColor();
+    }
+
+    //로딩 화면
+    public static void Loading()
+    {
+        Console.Clear();
+        Console.Write("Loading");
+        string str = ".";
+
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(75); //시간을 지연시킴
+            Console.Write(str);
+        }
+    }
+}
--- TxtRPG2/GameManager.cs
+++ TxtRPG2/GameManager.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace TxtRPG2
 {
-    internal class GameManager
+    class Program
     {
+        static void Main(string[] args)
+        {
+            // 게임 시작 인사
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다. \n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Select();
+        }
 
+        static void Select()
+        {
+            while (true)
+            {
+                ConsoleUtility.Loading();
+
+                Console.Clear();
+                // 메뉴 출력
+                Console.WriteLine("\n무엇을 하시겠습니까?");
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("0. 종료");
+                Console.Write("선택 (1, 2, 0): ");
+
+                string userPlay = Console.ReadLine();
+
+                // 입력한 값에 대한 출력
+
+                switch (userPlay)
+                {
+                    case "1":
+                        ShowStat();
+                        break;
+                    case "2":
+                        EnterBattle();
+                        break;
+                    case "0":
+                        Console.WriteLine("게임을 종료합니다. 안녕히 가세요!");
+                        return;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+                        break;
+                }
+            }
+        }
     }
 }
