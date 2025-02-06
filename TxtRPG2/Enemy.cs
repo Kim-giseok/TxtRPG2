13675832e52c846a08cedb5957d9c5eb54d21a88
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-06 17:15:29 +0900

    Feat: Enemy 클래스 생성
    인터페이스를 통해 IEnemy를 구현하고
    IEnemy를 상속받는 Enemy 클래스를 생성하였습니다.

---
+++ TxtRPG2/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    interface IEnemy
+    {
+        string Name { get; set; }
+        int Hp { get; set; }
+        int Atk { get; set; }
+        int Level { get; set; }
+        bool IsDead { get; set; }
+
+        void GetDamage(int a);
+    }
+    public class Enemy : IEnemy
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Level { get; set; }
+        public bool IsDead { get; set; }
+
+        public Enemy(string name, int health, int attackPower, int level)
+        {
+            this.Name = name;
+            this.Hp = health;
+            this.Atk = attackPower;
+            this.Level = level;
+            this.IsDead = false;
+        }
+
+        public void GetDamage(int a)
+        {
+            Console.WriteLine("적이 공격을 받았습니다.");
+            Hp -= a;
+            if (Hp <= 0)
+            {
+                IsDead = true;
+                Console.WriteLine("적이 죽었습니다.");
+            }
+        }
+    }
+}
