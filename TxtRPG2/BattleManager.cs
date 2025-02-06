73ad81918ae599b8d074bb0f19e4db1e3f92ba6d
Kim-giseok <belbeck@naver.com>
2025-02-06 17:04:01 +0900

    Feat: 생성자 구현
    현제 플래이어의 정보를 받고 등장할 수 있는 적들의 목록을 생성합니다.

--- TxtRPG2/BattleManager.cs
+++ TxtRPG2/BattleManager.cs
@@ -10,10 +10,17 @@ namespace TxtRPG2
     {
         Player player;
         Enemy[] Enemys;
+        public bool Victory { get => player.Hp != 0; }
 
         public BattleManager(Player player)
         {
-            
+            player = player;
+            Enemys =
+            [
+                new Enemy(2, "미니언", 15, 5),
+                new Enemy(3, "공허충", 10, 9),
+                new Enemy(5, "대포미니언", 25, 8)
+            ]
         }
 
         public void Battle()
