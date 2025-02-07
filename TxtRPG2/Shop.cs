7a4996e81de403c5ca3d1220575df4c057d71870
Kim-giseok <belbeck@naver.com>
2025-02-07 19:32:22 +0900

    Feat: Shop클래스 선언
    아이템을 사고 파는 Shop클래스의 틀을 만들었습니다.

---
+++ TxtRPG2/Shop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal class Shop
+    {
+        Player player;
+        public Item[] Items { get; private set; }
+
+        public Shop(Player player)
+        {
+        }
+
+        void ShowItems(bool buyMode = false)
+        {
+        }
+
+        public void ShopEnter()
+        {
+        }
+
+        void BuyItem()
+        {
+
+        }
+
+        void SellItem()
+        {
+
+        }
+    }
+}
