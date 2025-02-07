a9d34992fda572fbbe1f7d43d5b52656c4b6c592
Kim-giseok <belbeck@naver.com>
2025-02-07 17:47:43 +0900

    Feat: HpPotion 구현
    체력회복 아이템인 HpPotion의 클래스입니다.

---
+++ TxtRPG2/HpPotion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal class HpPotion:Item, IHpPotion
+    {
+        public int Hp { get; }
+
+        public HpPotion(string name, int hp, string description, int price, int isSoid) : base(name, description, price)
+        {
+            Hp = hp;
+        }
+
+        public override void ApearInfo(bool inShop = false)
+        {
+            Console.Write($"{Name}\t| 체력회복 + {Hp} | {Description}");
+            if (inShop)
+            {
+                if (IsSold)
+                {
+                    Console.WriteLine($"\t| 판매완료");
+                }
+                else
+                {
+                    Console.WriteLine($"\t| {Price} G");
+                }
+            }
+        }
+    }
+}
