4888a0927da930b188eff24a0a2bee86e923a4f4
Kim-giseok <belbeck@naver.com>
2025-02-07 16:01:02 +0900

    Fix: Player복구
    사라진 플레이어파일 복구했습니다.

---
+++ TxtRPG2/Player.cs
@@ -0,0 +1,49 @@
+﻿namespace TxtRPG2
+{
+
+    public class Player : Character //플레이어 클래스 . 캐릭터 인터페이스 상속
+    {
+        public string Job { get; }
+        public int Def { get; set; }
+        public int Gold { get; set; }
+
+        public Player(string name, int level, string job, int atk, int def, int hp, int gold)
+        {
+            Name = name;
+            Level = level;
+            Job = job;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Gold = gold;
+        }
+
+
+
+        public void Status() //플레이어의 상태를 보여주는 메서드
+        {
+            Console.WriteLine("상태 보기\n캐릭터의 정보가 표시됩니다.");
+            Console.WriteLine($"\nlv. {Level:D2}");
+            Console.WriteLine($"{Name} ({Job})");
+            Console.WriteLine($"공격력 : {Atk}");
+            Console.WriteLine($"방어력 : {Def}");
+            Console.WriteLine($"체력 : {Hp}");
+            Console.WriteLine($"Gold : {Gold} G");
+            Console.Write("\n0.나가기\n>> ");
+
+            Console.WriteLine(ConsoleUtility.GetInput); //입력값을 받아오는 메서드
+
+
+
+            /*  메인에서
+                플레이어 객체 생성
+                Player player = new Player(playerName, 1, "전사", 10, 5, 100, 1500);
+
+                상태 보기 메서드 호출
+                player.Status();
+            */
+
+        }
+
+    }
+}
