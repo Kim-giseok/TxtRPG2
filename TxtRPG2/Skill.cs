f75e55fef681827b1112a6787608b599adc9242b
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-07 17:18:44 +0900

    Feat : 스킬 클래스 생성, 생성자에 마나 추가
    스킬 클래스를 생성
    인터페이스와 생성자에 mp추가

---
+++ TxtRPG2/Skill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    public class Skill : Character
+
+    {
+        public int manacost { get; set; }
+        public Skill(int level, string name, int hp,int mp, int atk)
+            : base(level, name, hp, mp, atk)
+        {
+            
+        }
+        public void Skill1(Enemy enemy)
+        {
+            Console.WriteLine("스킬1 사용");
+            enemy.TakeDamage(Atk * 2);
+        }
+        public void Skill2(Enemy enemy)
+        {
+            Console.WriteLine("스킬2 사용");
+            enemy.TakeDamage(Atk * 3);
+        }
+        public void Skill3(Enemy enemy)
+        {
+            Console.WriteLine("스킬3 사용");
+            enemy.TakeDamage(Atk * 4);
+        }
+    }
+}
