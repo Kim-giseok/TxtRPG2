930e3efab56e1e4871caaaf1e76e49e6930fb0b4
KwonSumin <rbalstnals1920@gmail.com>
2025-02-07 10:01:34 +0900

    Refactor 코드 최적화

--- TxtRPG2/CinsoleUtity.cs
+++ TxtRPG2/CinsoleUtity.cs
@@ -16,13 +16,6 @@ static class ConsoleUtility
         }
     }
 
-    //글자 색 변경
-    public static void ColorWrite(string str, ConsoleColor color)
-    {
-        Console.ForegroundColor = color; //텍스트 컬러 설정
-        Console.WriteLine(str);
-        Console.ResetColor();
-    }
 
     //로딩 화면
     public static void Loading()
