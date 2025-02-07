08028de20ab34c241decf5d608d1cd9aecd1a1a6
yjyj713 <yjyj713@naver.com>
2025-02-07 10:44:47 +0900

    Feat : 동작 확인용 메인메서드 삭제
    플레이어 클래스 동작 확인용으로 만든 메인메서드 삭제

--- TxtRPG2/Program.cs
+++ TxtRPG2/Program.cs
@@ -8,32 +8,7 @@
             //깃 연동 확인(권수민)
             //깃 연동 확인(양원준)
             //222
-            //실행 확인 메인메서드
-            Console.Write("플레이어의 이름을 입력해주세요 : ");
-            string playerName = Console.ReadLine();
-
-            Player player = new Player(playerName, 1, "전사", 10, 5, 100, 1500); // Player 객체를 한 번만 생성
-
-            bool isRunning = true;
-
-            while (isRunning)
-            {
-                Console.Write("1. 상태 보기\n2. 데미지 받기\n>> ");
-                string input = Console.ReadLine();
-
-                if (input == "1")
-                {
-                    player.Status(); // 상태 보기
-                }
-                else if (input == "2")
-                {
-                    player.TakeDamage(10); // 데미지 받기
-                }
-                else
-                {
-                    Console.WriteLine("잘못된 입력입니다.");
-                }
-            }
+            Console.WriteLine("Hello, World!");
         }
     }
 }
