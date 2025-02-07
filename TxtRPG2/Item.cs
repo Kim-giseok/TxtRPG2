1e36512a22c0f775001a10b585390a9ffe5be426
Kim-giseok <belbeck@naver.com>
2025-02-07 17:08:30 +0900

    Feat: 아이템 클래스 생성
    아이템의 기본 정보를 담은 아이템 클래스를 생성했습니다.

---
+++ TxtRPG2/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal class Item
+    {
+        public string Name {  get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public string Description { get; }
+
+        public virtual void ApearInfo() { }
+    }
+}
