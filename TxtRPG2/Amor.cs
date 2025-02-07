ffeec2d34606a5c9f69905192f8bda24d035227d
Kim-giseok <belbeck@naver.com>
2025-02-07 17:35:37 +0900

    Feat; Amor클래스 구현
    방어구의 속성을 가진 Amor클래스를 구현했습니다.

---
+++ TxtRPG2/Amor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TxtRPG2
+{
+    internal class Amor : Item
+    {
+        public Amor(string name, int def, string description, int price, int isSoid) : base(name, 0, def, description, price)
+        { }
+
+        public override void ApearInfo(bool inShop = false)
+        {
+            Console.Write($"{Name}\t| 방어력 + {Def} | {Description}");
+            if (inShop)
+            {
+                if (IsSold)
+                {
+                    Console.WriteLine($"\t| 판매완료");
+                }
+                else
+                {
+                    Console.WriteLine($"\t| {Price} G");
+                }
+            }
+        }
+
+    }
+}
