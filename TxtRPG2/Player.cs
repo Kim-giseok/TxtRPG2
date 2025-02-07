0858564587a5f763eb6a985951e7615da74de2d1
yjyj713 <yjyj713@naver.com>
2025-02-07 10:35:17 +0900

    Feat : 캐릭터 클래스 추가
    캐릭터 클래스 추가시켜서 수정

--- TxtRPG2/Player.cs
+++ TxtRPG2/Player.cs
@@ -1,23 +1,42 @@
 ﻿namespace TxtRPG2
 {
-    public interface IPlayer //플레이어 인터페이스 정의
+    public interface ICharacter //ICharacter 인터페이스 정의
     {
-        string Name { get; }
-        int Level { get; }
-        int Atk { get; }
-        int Def { get; }
+        string Name { get; set; }
         int Hp { get; set; }
-        void GetDamage(int damage);
+        int Atk { get; set; }
+        int Level { get; set; }
+        void TakeDamage(int damage);
     }
-
-    public class Player : IPlayer //플레이어 클래스 . IPlayer 인터페이스 상속
+    public class Character : ICharacter //캐릭터 클래스 . ICharacter 인터페이스 상속 
     {
-        public string Name { get; }
-        public int Level { get; }
-        public string Job { get; }
-        public int Atk { get; }
-        public int Def { get; }
+        public string Name { get; set; }
         public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Level { get; set; }
+        public void TakeDamage(int Damage)
+        {
+            // 공격력의 ±10% 변동을 적용한 최종 공격력 계산
+            Damage = (int)Math.Ceiling(Damage * (1 + (new Random().NextDouble() * 0.2 - 0.1)));
+            //finalAtk = (int)Damage; // 데미지를 int로 변환
+            Console.WriteLine($"{Name}이 {Damage}만큼의 피해를 받았습니다.(기준 공격력: {Atk})");
+
+            Hp -= Damage;
+            if (Hp <= 0)
+            {
+                Hp = 0;
+                Console.WriteLine($"{Name}이 죽었습니다.");
+            }
+            else
+            {
+                Console.WriteLine($"남은 체력: {Hp}");
+            }
+        }
+    }
+    public class Player : Character //플레이어 클래스 . 캐릭터 인터페이스 상속
+    {
+        public string Job { get; }
+        public int Def { get; set; }
         public int Gold { get; set; }
 
         public Player(string name, int level, string job, int atk, int def, int hp, int gold)
@@ -31,25 +50,7 @@
             Gold = gold;
         }
 
-        public void GetDamage(int damage) //플레이어가 데미지 받을때 호출되는 메서드
-        {
-            Console.WriteLine($"HP {Hp} -> {Hp - damage}");
-            Hp -= damage;
-            if (Hp < 0)
-            {
-                Hp = 0;
-            }
-            Console.Write("0. 메뉴화면으로\n>> ");
-            string input = Console.ReadLine();
-            if (input == "0")
-            {
-                Console.WriteLine("메뉴화면으로 나갑니다.");
-            }
-            else
-            {
-                Console.WriteLine("잘못된 입력입니다.");
-            }
-        }
+
 
         public void Status() //플레이어의 상태를 보여주는 메서드
         {
