1f10d96aa9faf6844e42bef78f60e9606f39681b
KwonSumin <rbalstnals1920@gmail.com>
2025-02-07 14:52:48 +0900

    feat : GameManager 클래스 구현
    GameManager 클래스 구현

--- TxtRPG2/GameManager.cs
+++ TxtRPG2/GameManager.cs
@@ -9,14 +9,9 @@ namespace TxtRPG2
 {
     class GameManager
     {
-        static void Main(string[] args)
-        {
-            // 게임 시작 인사
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다. \n이제 전투를 시작할 수 있습니다.");
-            StartScene();
-        }
 
-        static void StartScene()
+
+        public void StartScene()
         {
             while (true)
             {   //로딩 중 표시
@@ -24,19 +19,28 @@ namespace TxtRPG2
 
                 Console.Clear();
                 // 메뉴 출력
-                Console.WriteLine("\n무엇을 하시겠습니까?");
                 Console.WriteLine("1. 상태 보기");
                 Console.WriteLine("2. 전투 시작");
+                Console.WriteLine("0. 종료");
                 Console.Write("선택 (1, 2): ");
 
-                if(ConsoleUtility.GetInput(out int input, 1, 2))
-                {
-                    break;
-                }
+                int input = ConsoleUtility.GetInput(1, 2);
 
                 // 입력한 값에 대한 출력
 
-                
+                switch (input)
+                {
+                    case 1:
+                        ShowStat();
+                        break;
+                    case 2:
+                        EnterBattle();
+                        break;
+
+                        // 입력한 값에 대한 출력
+
+
+                }
             }
         }
         public void ShowStat()
@@ -44,16 +48,21 @@ namespace TxtRPG2
             ConsoleUtility.Loading();
 
             Console.Clear();
-            player.Status();
 
             ConsoleUtility.GetInput(0, 0);
             StartScene();
+
+
+
         }
         public void EnterBattle()
         {
             ConsoleUtility.Loading();
 
             Console.Clear();
+
+            ConsoleUtility.GetInput(0, 0);
+            StartScene();
         }
     }
 }
