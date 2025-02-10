c92c1977dfdf538712434fdc68de20218d6b54bc
Kim-giseok <belbeck@naver.com>
2025-02-10 16:37:27 +0900

    Feat: MpPotion구현
    플레이어의 마력을 회복하는 MpPotion클래스를 구현했습니다.

---
+++ TxtRPG2/Item/Potion/MpPotion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal interface IMpPotion
+    {
+        public int Mp { get; }
+    }
+
+    internal class MpPotion : Potion, IMpPotion
+    {
+        public int Mp { get; }
+
+        public MpPotion(string name, int Mp, string description, int price) : base(name, description, price)
+        {
+            this.Mp = Mp;
+        }
+
+        public MpPotion(MpPotion potion) : base(potion)
+        {
+            Mp = potion.Mp;
+        }
+
+        public override void ApearInfo(bool select = false)
+        {
+            int sl = Console.CursorLeft;
+            base.ApearInfo(select);
+            Console.SetCursorPosition(sl + 18, Console.CursorTop - 1);
+            Console.Write($"마력회복\t+ {Mp}");
+            Console.SetCursorPosition(0, Console.CursorTop + 1);
+        }
+
+        public override void Use(Player player)
+        {
+            int cnt = count;
+            base.Use(player);
+
+            if (cnt != 0)
+            {
+                int baseMp = player.Mp;
+                player.Mp += Mp;
+                if (player.Mp > 100)
+                {
+                    player.Mp = 100;
+                }
+                Console.WriteLine($"{player.Name}은 체력을 {player.Mp - baseMp}만큼 회복했습니다.");
+            }
+        }
+    }
+}
