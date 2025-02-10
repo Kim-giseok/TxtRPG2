bf272998a5898b30968e0394d721c5959c28e63b
Kim-giseok <belbeck@naver.com>
2025-02-10 10:05:02 +0900

    Feat: 인벤토리 구현
    인벤토리 클래스와 인벤토리 안의 아이템을 보여주는 기능을 구현 했습니다.

---
+++ TxtRPG2/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG2
+{
+    internal class Inventory
+    {
+        public List<Item> Equips { get; set; }
+        public Weapon EWeapon { get; set; }
+        public Amor EAmor { get; set; }
+
+        public Inventory()
+        {
+            Equips = new List<Item>();
+        }
+
+        public void ShowInfo (bool equip = false)
+        {
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine();
+            for (int i = 0; i < Equips.Count; i++)
+            {
+                Console.Write("- ");
+                if (EWeapon == Equips[i] || EAmor == Equips[i])
+                {
+                    Console.Write(" [E]");
+                }
+                Equips[i].ApearInfo(true);
+            }
+        }
+    }
+}
